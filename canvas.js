b1661499ab8baaf3dd999c81f826e8758ac4fa10
Dmitry <n0thingss@yandex.ru>
2019-05-04 06:43:40 +0300

    add some images

--- canvas.js
+++ canvas.js
@@ -1,10 +1,22 @@
 var canvas = document.createElement('canvas');
 canvas.id = "collage";
-canvas.wigth = 500;
-canvas.height = 500;
+canvas.wigth = 400;
+canvas.height = 400;
 canvas.style.border = "1px solid grey";
 canvas.style.margin = "auto";
 canvas.style.display = "block";
 document.body.appendChild(canvas);
 var elem = document.getElementById("collage");
 var ctx = canvas.getContext("2d");
+var img1 = new Image;
+var img2 = new Image;
+var img3 = new Image;
+var img4 = new Image;
+img1.src = 'https://source.unsplash.com/collection/1127163/300x200';
+img2.src = 'https://source.unsplash.com/collection/1127164/300x200';
+img3.src = 'https://source.unsplash.com/collection/1127165/300x200';
+img4.src = 'https://source.unsplash.com/collection/1127166/300x200';
+img1.onload = function() { ctx.drawImage(img1, 0, 0); }
+img2.onload = function() { ctx.drawImage(img2, 200, 200); }
+img3.onload = function() { ctx.drawImage(img3, 0, 200); }
+img4.onload = function() { ctx.drawImage(img4, 200, 0); }
