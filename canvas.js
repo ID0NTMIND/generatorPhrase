f194ed218d3803c4247afc7607a33da078992ef9
Dmitry <n0thingss@yandex.ru>
2019-05-04 06:49:09 +0300

    add working download button

--- canvas.js
+++ canvas.js
@@ -12,11 +12,31 @@ var img1 = new Image;
 var img2 = new Image;
 var img3 = new Image;
 var img4 = new Image;
-img1.src = 'https://source.unsplash.com/collection/1127163/300x200';
-img2.src = 'https://source.unsplash.com/collection/1127164/300x200';
-img3.src = 'https://source.unsplash.com/collection/1127165/300x200';
-img4.src = 'https://source.unsplash.com/collection/1127166/300x200';
+img1.crossOrigin = 'anonymous';
+img2.crossOrigin = 'anonymous';
+img3.crossOrigin = 'anonymous';
+img4.crossOrigin = 'anonymous';
+img1.src = 'http://source.unsplash.com/collection/1127163/300x200';
+img2.src = 'http://source.unsplash.com/collection/1127164/300x200';
+img3.src = 'http://source.unsplash.com/collection/1127165/300x200';
+img4.src = 'http://source.unsplash.com/collection/1127166/300x200';
 img1.onload = function() { ctx.drawImage(img1, 0, 0); }
 img2.onload = function() { ctx.drawImage(img2, 200, 200); }
 img3.onload = function() { ctx.drawImage(img3, 0, 200); }
 img4.onload = function() { ctx.drawImage(img4, 200, 0); }
+
+var ref = document.createElement('a');
+var button = document.createElement('button');
+button.appendChild(ref);
+document.body.appendChild(button);
+ref.id = "down-btn";
+ref.href = "#";
+ref.textContent = "Download collage";
+ref.download = "collage.png";
+ref.addEventListener('click', function(e) {
+    var dataURL = canvas.toDataURL();
+    ref.href = dataURL;
+})
+button.style.margin = "auto";
+button.style.display = "block";
+button.style.marginTop = "40px"
